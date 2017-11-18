ebfb786ffa81acc2e5594eccbe10ede8590e1ce5
Your Name <you@example.com>
2017-11-18 17:13:59 -0500

    añadiendo funcionalidades, falta scroll

--- js/app.js
+++ js/app.js
@@ -3,40 +3,53 @@ window.addEventListener('load',function() {
   var btnSave = document.getElementById('save');
   var containerContact = document.getElementById('container-contact');
   var contadorNumb = document.getElementById('number-cont');
+  var max = 140;
 
+//   nameContact.addEventListener("keyup", function(){
+//   var contador = '';
+//   contador = nameContact.value.length;
+//   contadorNumb.value = max - contador;
 
-nameContact.addEventListener("keyup", function(){
-  var contador = '';
-  contador = nameContact.value.length;
-    contadorNumb.value = 140 - contador;
-})
+// })
+
 
   btnSave.addEventListener('click', function(event){
     if(nameContact.value){
       event.preventDefault();
     var newContact = document.createElement('li'); //este es el padre
+    containerContact.insertBefore(newContact,containerContact.firstElementChild);
+
 
     var newName = document.createElement('span');
     newName.setAttribute('class', 'contact-name');
     newName.textContent = nameContact.value;
     newContact.appendChild(newName);
-    nameContact.value = '';
-    }
+    nameContact.value = ''; 
+    contadorNumb.value = max;
+    };
 
+});
+nameContact.addEventListener("keyup", function(){
+  var contador = '';
+  contador = nameContact.value.length;
+  contadorNumb.value = max - contador;
+  if (contador>140) {
+    btnSave.disabled = true;
+    contadorNumb.style.color = 'red'
 
-  //malogranding
+  }else if (contador>=130){
+    btnSave.disabled = false;
+    contadorNumb.style.color = 'pink';
+    
+  }else if (contador<130 && contador>120){
+    btnSave.disabled = false;
+    contadorNumb.style.color = 'yellow';
+  
+  }else{
+    btnSave.disabled=false;
+    contadorNumb.style.color = 'black';
+  }
 
-/*if (text.length.value === 0)
-   {
-    save.disabled=true;
-   }
-
-else{
-      save.disabled=false;
-      }
-}*/
-
-  containerContact.insertBefore(newContact,containerContact.firstElementChild);
 });
 
 });
